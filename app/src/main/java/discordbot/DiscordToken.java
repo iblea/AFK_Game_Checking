6b0c76eae997308cb143e443d5475860b92254fd
pangnem <sprnd645@gmail.com>
2022-06-05 19:54:08 +0900

    test: DiscordBotToken
    테스트를 위해 EqualsAndHashCode 어노테이션 추가
    
    See Also:
    - VO(Value Object)
    - 불변객체

--- app/src/main/java/discordbot/DiscordToken.java
+++ app/src/main/java/discordbot/DiscordToken.java
@@ -1,27 +1,28 @@
 package discordbot;
 
-import java.io.IOException;
-import java.nio.file.Paths;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
+@EqualsAndHashCode
 public class DiscordToken {
 
-	@Getter
-	private final String botToken;
+    @Getter
+    private final String botToken;
 
-	public DiscordToken(String path) throws IOException {
-		this.botToken = readToken(path);
+    public DiscordToken(String path) throws IOException {
+        this.botToken = readToken(path);
     }
 
-	/**
-	 * @brief Path argument를 통해 파일에서 토큰을 추출해오는 함수
-	 * @param path token이 들어있는 path 경로
-	 * @return String
-	 */
-	private String readToken(String path) throws IOException{
-		return Files.readString(Paths.get(path));
-	}
+    /**
+     * @param path token이 들어있는 path 경로
+     * @return String
+     * @brief Path argument를 통해 파일에서 토큰을 추출해오는 함수
+     */
+    private String readToken(String path) throws IOException {
+        return Files.readString(Paths.get(path));
+    }
 }
