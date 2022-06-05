e650461d00ddeb4e135c993241c857dd60f67e4d
pangnem <sprnd645@gmail.com>
2022-06-05 20:07:36 +0900

    test: BotEvent - setOffline, setOnline

---
+++ app/src/test/java/discordbot/BotEventTest.java
@@ -0,0 +1,26 @@
+package discordbot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BotEventTest {
+
+    @Test
+    void setOffline() {
+        BotEvent botEvent = new BotEvent(BotEvent.Status.ONLINE);
+
+        botEvent.setOffline();
+
+        assertThat(botEvent).isEqualTo(new BotEvent(BotEvent.Status.OFFLINE));
+    }
+
+    @Test
+    void setOnline() {
+        BotEvent botEvent = new BotEvent(BotEvent.Status.OFFLINE);
+
+        botEvent.setOnline();
+
+        assertThat(botEvent).isEqualTo(new BotEvent(BotEvent.Status.ONLINE));
+    }
+}
