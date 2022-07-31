bb63fa1c04f0a9763faea6aa8fa9a485621ff78a
jhh9232 <ghkd01212@gmail.com>
2022-08-01 01:13:20 +0900

    test: 불필요 테스트 코드 제거

--- app/src/test/java/discordbot/BotEventTest.java
+++ app/src/test/java/discordbot/BotEventTest.java
@@ -7,20 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BotEventTest {
 
 	@Test
-	void setOffline() {
-		BotEvent botEvent = new BotEvent(BotEvent.Status.ONLINE);
-
-		botEvent.setOffline();
-
-		assertThat(botEvent).isEqualTo(new BotEvent(BotEvent.Status.OFFLINE));
-	}
-
-	@Test
-	void setOnline() {
-		BotEvent botEvent = new BotEvent(BotEvent.Status.OFFLINE);
-
-		botEvent.setOnline();
-
-		assertThat(botEvent).isEqualTo(new BotEvent(BotEvent.Status.ONLINE));
+	void TestExample() {
+		assertThat("").isEqualTo("");
 	}
 }
