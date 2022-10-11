b3ef32a5b11ac13a557c5af85f397902881de462
jhh9232 <ghkd01212@gmail.com>
2022-10-12 00:22:07 +0900

    refactor: if, else 구조의 명령어 로직을 ENUM 형식으로 변경

--- app/src/main/java/discordbot/BotCommand.java
+++ app/src/main/java/discordbot/BotCommand.java
@@ -1,62 +1,81 @@
 package discordbot;
 
+import java.util.Arrays;
+
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
-public class BotCommand
-{
-    final String HELP = "help";
-    final String USER_ADD = "gcadd";
-    final String USER_DEL = "gcdel";
+public class BotCommand {
 
 	private MessageReceivedEvent event;
 
-    public BotCommand(MessageReceivedEvent event) {
-        this.event = event;
-    }
-
+	public BotCommand(MessageReceivedEvent event) {
+		this.event = event;
+	}
 
 	public int botCommandContent(String msg) {
+		// msg를 아예 받지 못함
+		if (msg.length() < 1) {
+			return -1;
+		}
+
 		if (msg.charAt(0) != '!') {
 			return 0;
 		}
 
 		msg = msg.substring(1);
+		System.out.println(msg);
 
-		switch(msg) {
-			case HELP:
-                help_command(msg);
+		return COMMAND_TYPE.calculate(msg, this.event);
+	}
+
+	public void cmdContent(MessageReceivedEvent event) {
+
+	}
+
+	public enum COMMAND_TYPE {
+		HELP("help") {
+			@Override
+			public int apply(String msg, MessageReceivedEvent event) {
+				String help_content = "help command test";
+				MessageChannel channel = event.getChannel();
+				channel.sendMessage(help_content).queue();
 				return 1;
-			case USER_ADD:
-				add_user();
+			}
+		},
+		USER_ADD("gcadd") {
+			@Override
+			public int apply(String msg, MessageReceivedEvent event) {
+
 				return 2;
-			case USER_DEL:
-				del_user();
+			}
+		},
+		USER_DEL("gcdel") {
+			@Override
+			public int apply(String msg, MessageReceivedEvent event) {
+
 				return 3;
+			}
+		},
+
+		;
+
+		private final String command_text;
+
+		private COMMAND_TYPE(String command_text) {
+			this.command_text = command_text;
 		}
 
-		return 0;
+		public abstract int apply(String msg, MessageReceivedEvent event);
+
+		public static int calculate(String msg, MessageReceivedEvent event) {
+			COMMAND_TYPE cmd_type = Arrays.stream(values())
+					.filter(v -> v.command_text.equals(msg))
+					.findFirst()
+					.orElseThrow(IllegalArgumentException::new);
+
+			return cmd_type.apply(msg, event);
+		}
 	}
 
-    private void help_command(String msg)
-    {
-		String help_content = "help test";
-		MessageChannel channel = event.getChannel();
-		channel.sendMessage(help_content).queue();
-    }
-
-	private void add_user()
-	{
-
-	}
-
-	private void del_user()
-	{
-
-	}
-
-    public void cmdContent(MessageReceivedEvent event)
-    {
-
-    }
 }
