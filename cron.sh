06ea59ccaf478d10277b8d3fba925b0146c225b4
jdh9232 <ghkd01212@gmail.com>
2023-08-16 01:32:44 +0900

    crontab 경로 설정

--- cron.sh
+++ cron.sh
@@ -3,7 +3,11 @@
 
 BOT_PATH=/path/to/start.sh
 
+botdir=$(dirname $BOT_PATH)
 proc=$(ps -aef | grep "bot.py" | grep -v "grep")
 if [ "$proc" = "" ]; then
-    nohup $BOT_PATH &
+    if [ -f $BOT_PATH ]; then
+        cd "$botdir"
+        nohup $BOT_PATH &
+    fi
 fi
