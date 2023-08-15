17178d26519e0f96a9075313c611ebf86050036f
jdh9232 <ghkd01212@gmail.com>
2023-08-16 01:52:59 +0900

    실행권한 체크

--- cron.sh
+++ cron.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME="bot.py"
 botdir=$(dirname $BOT_PATH)
 proc=$(ps -aef | grep "$SCRIPT_NAME" | grep -v "grep")
 if [ "$proc" = "" ]; then
-    if [ -f $BOT_PATH ]; then
+    if [ -x $BOT_PATH ]; then
         cd "$botdir"
         nohup $BOT_PATH &
     fi
