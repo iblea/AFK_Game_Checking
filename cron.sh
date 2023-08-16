395baab20dbf130ac535175e0168325c93477c47
jdh9232 <ghkd01212@gmail.com>
2023-08-17 08:38:15 +0900

    스크립트 설정 변수 가변 적용

--- cron.sh
+++ cron.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 
-BOT_PATH=/path/to/start.sh
 SCRIPT_NAME="bot.py"
+SH_CONFIG="script_config"
+curpath=$(dirname $(realpath $0))
 
-botdir=$(dirname $BOT_PATH)
+if [ "$curpath" == "" ]; then
+    exit 1
+fi
+
+if [ ! -f "$curpath/$SH_CONFIG" ]; then
+    exit 1
+fi
+
+BOT_PATH=$curpath/start.sh
+source $curpath/$SH_CONFIG
+
+if [ -z "$SCRIPT_NAME" ]; then
+    exit 1
+fi
+
 proc=$(ps -aef | grep "$SCRIPT_NAME" | grep -v "grep")
 if [ "$proc" = "" ]; then
     if [ -x $BOT_PATH ]; then
-        cd "$botdir"
+        cd "$curpath"
         nohup $BOT_PATH &
     fi
 fi
--- start.sh
+++ start.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-venv_name=discordbot
-main_script=bot.py
+SH_CONFIG="script_config"
+
+curpath=$(dirname $(realpath $0))
+cd "$curpath"
+
+if [ ! -f "$curpath/$SH_CONFIG" ]; then
+    exit 1
+fi
+
+source $curpath/$SH_CONFIG
+
+if [ -z "$VENV_NAME" ]; then
+    echo "no VENV_NAME variable"
+    exit 1
+fi
+
+if [ -z "$SCRIPT_NAME" ]; then
+    echo "no SCRIPT_NAME variable"
+    exit 1
+fi
+
 
 # install
-# python3 -m venv $venv_name
+# python3 -m venv $VENV_NAME
 # if not create 'activate' file
-# python3 -m venv --without-pip $venv_name
-# curl https://bootstrap.pypa.io/get-pip.py | $venv_name/bin/python3
+# python3 -m venv --without-pip $VENV_NAME
+# curl https://bootstrap.pypa.io/get-pip.py | $VENV_NAME/bin/python3
 
-source $venv_name/bin/activate
+source $VENV_NAME/bin/activate
 
 # echo "pip list"
 # pip freeze
 # pip install -r pip_freeze.txt
-# $venv_name/bin/pip3 install discord
+# $VENV_NAME/bin/pip3 install discord
 
 echo "venv activate"
-$venv_name/bin/python $main_script
+$VENV_NAME/bin/python $SCRIPT_NAME
 
 deactivate
 echo "venv deactivate"
